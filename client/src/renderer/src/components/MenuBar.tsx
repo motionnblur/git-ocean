3b4916956dfa543ba4228de105b92571f4e5d05f
carbonfiberdev <carbonfiberdev@gmail.com>
2025-04-24 12:17:21 +0300

    Remove bg image
    And some refactor

--- client/src/renderer/src/components/MenuBar.tsx
+++ client/src/renderer/src/components/MenuBar.tsx
@@ -1,17 +1,17 @@
-import { AppBar, Box, IconButton, Toolbar } from '@mui/material'
+import { Box, IconButton, Stack, Toolbar } from '@mui/material'
 import { JSX } from 'react'
 import FolderIcon from '@mui/icons-material/Folder'
 
 export default function MenuBar(): JSX.Element {
   return (
-    <Box sx={{ width: '100%' }}>
-      <AppBar position="static">
+    <Box sx={{ width: '100%', backgroundColor: '#0b305d' }}>
+      <Stack direction="row">
         <Toolbar variant="dense">
           <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <FolderIcon />
           </IconButton>
         </Toolbar>
-      </AppBar>
+      </Stack>
     </Box>
   )
 }
