3b27f8c1a8a20cf2d34e1ce333db046b622098ec
carbonfiberdev <carbonfiberdev@gmail.com>
2025-04-24 13:26:19 +0300

    Add main window

--- client/src/renderer/src/App.tsx
+++ client/src/renderer/src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import MenuBar from './components/MenuBar'
 import Popup from './components/Popup'
 import ToolBar from './components/ToolBar'
+import MainWindow from './components/MainWindow'
 
 function App(): React.JSX.Element {
   const [open, setOpen] = useState<boolean>(true)
@@ -15,6 +16,7 @@ function App(): React.JSX.Element {
       <Stack direction="column">
         <MenuBar />
         <ToolBar />
+        <MainWindow />
       </Stack>
       <Popup
         open={open}
---
+++ client/src/renderer/src/components/MainWindow.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material'
+import { JSX } from 'react'
+
+export default function MainWindow(): JSX.Element {
+  return (
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: '#222831'
+      }}
+    ></Box>
+  )
+}
