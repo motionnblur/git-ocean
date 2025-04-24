eac240a9c8978164959a16f47e8f6f294735200a
carbonfiberdev <carbonfiberdev@gmail.com>
2025-04-24 12:35:47 +0300

    Refactor menubar

--- client/src/renderer/src/components/MenuBar.tsx
+++ client/src/renderer/src/components/MenuBar.tsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, Stack, Toolbar } from '@mui/material'
+import { Box, Container, IconButton, Stack } from '@mui/material'
 import { JSX } from 'react'
 import FolderIcon from '@mui/icons-material/Folder'
 
@@ -6,11 +6,11 @@ export default function MenuBar(): JSX.Element {
   return (
     <Box sx={{ width: '100%', backgroundColor: '#0b305d' }}>
       <Stack direction="row">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <FolderIcon />
+        <Container>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <FolderIcon fontSize="small" />
           </IconButton>
-        </Toolbar>
+        </Container>
       </Stack>
     </Box>
   )
