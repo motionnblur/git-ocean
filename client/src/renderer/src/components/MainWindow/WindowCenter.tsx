a1c4602f0c7739b4403c0a2a72d766f6399978fa
carbonfiberdev <carbonfiberdev@gmail.com>
2025-04-25 00:59:16 +0300

    Improve terminal

--- client/src/renderer/src/components/MainWindow/WindowCenter.tsx
+++ client/src/renderer/src/components/MainWindow/WindowCenter.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/material'
-import { JSX, useState } from 'react'
+import { JSX, useEffect, useState } from 'react'
 import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui'
 
 export default function WindowCenter(): JSX.Element {
@@ -7,6 +7,31 @@ export default function WindowCenter(): JSX.Element {
     // eslint-disable-next-line react/jsx-key
     <TerminalOutput>Output test</TerminalOutput>
   ])
+
+  useEffect(() => {
+    const handleOutput = (data: string): void => {
+      setTerminalLineData((prev) => [
+        ...prev,
+        <TerminalOutput key={prev.length}>{data}</TerminalOutput>
+      ])
+    }
+
+    const handleExit = (code: number): void => {
+      setTerminalLineData((prev) => [
+        ...prev,
+        <TerminalOutput key={prev.length}>{`Process exited with code ${code}`}</TerminalOutput>
+      ])
+    }
+
+    window.electron.onCommandOutput(handleOutput)
+    window.electron.onCommandExit(handleExit)
+
+    // Optional cleanup
+    return () => {
+      // You may want to implement "off" methods in preload for proper cleanup
+    }
+  }, [])
+
   return (
     <Box sx={{ flexGrow: 1, backgroundColor: '#dbd8e3' }}>
       <Terminal
