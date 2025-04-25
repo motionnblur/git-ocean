a9b79ea3bf2521b35ea1c221759f7c1427ab8739
motionnblur <carbonfiberdev@gmail.com>
2025-04-25 16:40:04 +0300

    Add list

--- client/src/renderer/src/components/RepoView/RepoView.tsx
+++ client/src/renderer/src/components/RepoView/RepoView.tsx
@@ -1,6 +1,25 @@
-import { Box } from '@mui/material'
+import { Box, ListItem, ListItemButton, ListItemText } from '@mui/material'
 import { JSX } from 'react'
+import { FixedSizeList, ListChildComponentProps } from 'react-window'
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function renderRow(props: ListChildComponentProps) {
+  const { index, style } = props
+
+  return (
+    <ListItem style={style} key={index} component="div" disablePadding>
+      <ListItemButton>
+        <ListItemText primary={`Item ${index + 1}`} />
+      </ListItemButton>
+    </ListItem>
+  )
+}
 export default function RepoView(): JSX.Element {
-  return <Box sx={{ width: '100%', height: '100%', backgroundColor: '#171130' }}></Box>
+  return (
+    <Box sx={{ width: '100%', height: '100%', backgroundColor: '#171130' }}>
+      <FixedSizeList height={400} width={360} itemSize={46} itemCount={200} overscanCount={5}>
+        {renderRow}
+      </FixedSizeList>
+    </Box>
+  )
 }
