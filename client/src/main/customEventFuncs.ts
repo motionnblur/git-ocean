2bed0ec853f5a76bc942b98aa46b336fbd8ba479
motionnblur <carbonfiberdev@gmail.com>
2025-04-25 18:39:44 +0300

    Get all commit data

--- client/src/main/customEventFuncs.ts
+++ client/src/main/customEventFuncs.ts
@@ -1,14 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getGitCommitData = async (execPromise: any): Promise<any> => {
-  const { stdout: commitData } = await execPromise('git log -1 --format=%H')
-  const { stdout: commitName } = await execPromise('git log -1 --format=%s')
-  const { stdout: authorData } = await execPromise('git log -1 --format=%an')
-  const { stdout: dateData } = await execPromise('git log -1 --format=%ad')
+export const getGitCommitData = async (execPromise: any): Promise<any[]> => {
+  const { stdout: commitData } = await execPromise('git log --format=%H')
+  const { stdout: commitNames } = await execPromise('git log --format=%s')
+  const { stdout: authorData } = await execPromise('git log --format=%an')
+  const { stdout: dateData } = await execPromise('git log --format=%ad')
 
-  return {
-    commitData: commitData.trim(),
-    commitName: commitName.trim(),
-    authorData: authorData.trim(),
-    dateData: dateData.trim()
-  }
+  const commitDataArray = commitData.trim().split('\n')
+  const commitNamesArray = commitNames.trim().split('\n')
+  const authorDataArray = authorData.trim().split('\n')
+  const dateDataArray = dateData.trim().split('\n')
+
+  return commitDataArray.map((_, index) => ({
+    commitData: commitDataArray[index],
+    commitName: commitNamesArray[index],
+    authorData: authorDataArray[index],
+    dateData: dateDataArray[index]
+  }))
 }
