50f141a48971e47d706504e95442add52494f86d
motionnblur <carbonfiberdev@gmail.com>
2025-04-26 18:05:28 +0300

    Open close commit window

--- client/src/renderer/src/components/MainWindow/WindowRight.tsx
+++ client/src/renderer/src/components/MainWindow/WindowRight.tsx
@@ -1,11 +1,26 @@
 import { Box } from '@mui/material'
-import { JSX } from 'react'
+import { JSX, useEffect, useState } from 'react'
 import CommitWindow from './CommitWindow'
+import { eventManager } from '@renderer/class/EventManager'
 
 export default function WindowRight(): JSX.Element {
+  const [openCommitWindow, setOpenCommitWindow] = useState(false)
+  const handleCommitWindowOpen = (): void => {
+    setOpenCommitWindow(true)
+  }
+  useEffect(() => {
+    eventManager.on('open-commit-window', handleCommitWindowOpen)
+    return () => {
+      eventManager.off('open-commit-window', handleCommitWindowOpen)
+    }
+  }, [])
   return (
-    <Box sx={{ width: '280px', height: 1, backgroundColor: '#222831' }}>
-      <CommitWindow />
-    </Box>
+    <>
+      {openCommitWindow && (
+        <Box sx={{ width: '280px', height: 1, backgroundColor: '#222831' }}>
+          <CommitWindow />
+        </Box>
+      )}
+    </>
   )
 }
